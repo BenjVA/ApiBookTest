321629ebf901397341c98df10b0b2d6e2423f04e
BenjVA <ben.valette@live.fr>
2023-09-15 12:47:49 +0200

    add cache and pagination in author and book controllers

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -5,23 +5,44 @@ namespace App\Controller;
 use App\Entity\Author;
 use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class AuthorController extends AbstractController
 {
+    /**
+     * @throws InvalidArgumentException in cache->get()
+     */
     #[Route('/api/authors', name: 'app_authors', methods: ['GET'])]
-    public function getAuthors(AuthorRepository $authorRepository, SerializerInterface $serializer): JsonResponse
+    public function getAuthors(AuthorRepository $authorRepository, SerializerInterface $serializer, Request $request, TagAwareCacheInterface $cache, PaginatorInterface $paginator): JsonResponse
     {
-        $authorList = $authorRepository->findAll();
-        $jsonAuthorList = $serializer->serialize($authorList, 'json', ['groups' => 'getAuthors']);
+        $authors = $authorRepository->findAll();
+
+        $offset = $request->query->getInt('offset', 1);
+        $limit = $request->query->getInt('limit', 5);
+
+        $idCache = 'getAuthors-' . $offset . '-' . $limit;
+
+        $jsonAuthorList = $cache->get($idCache, function (ItemInterface $item) use ($paginator, $authors, $offset, $limit, $serializer) {
+            echo('L\'élément n\'est pas encore en cache !'); // displays on postman if we went through the function
+            $item->tag('authorsCache')
+                ->expiresAfter(60);
+            $authorsPaginated = $paginator->paginate($authors, $offset, $limit);
+
+            return $serializer->serialize($authorsPaginated, 'json', ['groups' => 'getAuthors']);
+        });
 
         return new JsonResponse($jsonAuthorList, Response::HTTP_OK, [], true);
     }
@@ -34,9 +55,13 @@ class AuthorController extends AbstractController
         return new JsonResponse($jsonAuthor, Response::HTTP_OK, [], true);
     }
 
+    /**
+     * @throws InvalidArgumentException in invalidateTags
+     */
     #[Route('/api/authors/{id}', name: 'app_authors_delete', methods: ['DELETE'])]
-    public function deleteAuthor(Author $author, EntityManagerInterface $entityManager): JsonResponse
+    public function deleteAuthor(Author $author, EntityManagerInterface $entityManager, TagAwareCacheInterface $cache): JsonResponse
     {
+        $cache->invalidateTags(['authorsCache']);
         $entityManager->remove($author);
         $entityManager->flush();
 
@@ -44,11 +69,12 @@ class AuthorController extends AbstractController
     }
 
     #[Route('/api/authors', name: 'app_authors_create', methods: ['POST'])]
-    public function createAuthor(Request $request,
-                               SerializerInterface $serializer,
-                               EntityManagerInterface $entityManager,
-                               UrlGeneratorInterface $urlGenerator,
-                               ValidatorInterface $validator
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour créer un auteur.')]
+    public function createAuthor(Request                $request,
+                                 SerializerInterface    $serializer,
+                                 EntityManagerInterface $entityManager,
+                                 UrlGeneratorInterface  $urlGenerator,
+                                 ValidatorInterface     $validator
     ): JsonResponse
     {
         $author = $serializer->deserialize($request->getContent(), Author::class, 'json');
@@ -68,12 +94,16 @@ class AuthorController extends AbstractController
         return new JsonResponse($jsonAuthor, Response::HTTP_CREATED, ['Location' => $location], true);
     }
 
+    /**
+     * @throws InvalidArgumentException in invalidateTags
+     */
     #[Route('/api/authors/{id}', name: 'app_authors_update', methods: ['PATCH'])]
-    public function updateAuthor(Request $request,
-                               SerializerInterface $serializer,
-                               Author $currentAuthor,
-                               EntityManagerInterface $entityManager,
-                               ValidatorInterface $validator
+    public function updateAuthor(Request                $request,
+                                 SerializerInterface    $serializer,
+                                 Author                 $currentAuthor,
+                                 EntityManagerInterface $entityManager,
+                                 ValidatorInterface     $validator,
+                                 TagAwareCacheInterface $cache
     ): JsonResponse
     {
         $updatedAuthor = $serializer->deserialize(
@@ -88,6 +118,7 @@ class AuthorController extends AbstractController
             return new JsonResponse($serializer->serialize($errors, 'json'), Response::HTTP_BAD_REQUEST, [], true);
         }
 
+        $cache->invalidateTags(['authorsCache']);
         $entityManager->persist($updatedAuthor);
         $entityManager->flush();
 
--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -7,6 +7,7 @@ use App\Repository\AuthorRepository;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,17 +18,36 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class BookController extends AbstractController
 {
+    /**
+     * @throws InvalidArgumentException in cache
+     */
     #[Route('/api/books', name: 'app_books', methods: ['GET'])]
-    public function getBooks(BookRepository $bookRepository, SerializerInterface $serializer, Request $request, PaginatorInterface $paginator): JsonResponse
+    public function getBooks(BookRepository         $bookRepository,
+                             SerializerInterface    $serializer,
+                             Request                $request,
+                             PaginatorInterface     $paginator,
+                             TagAwareCacheInterface $cache): JsonResponse
     {
         $books = $bookRepository->findAll();
 
-        $bookList = $paginator->paginate($books, $request->query->getInt('page', 1), 3);
+        $offset = $request->query->getInt('offset', 1);
+        $limit = $request->query->getInt('limit', 5);
 
-        $jsonBookList = $serializer->serialize($bookList, 'json', ['groups' => 'getBooks']);
+        $idCache = 'getBooks-' . $offset . '-' . $limit;
+
+        $jsonBookList = $cache->get($idCache, function (ItemInterface $item) use ($paginator, $books, $offset, $limit, $serializer) {
+            echo('L\'élément n\'est pas encore en cache !'); // displays on postman if we went through the function
+            $item->tag('booksCache')
+                ->expiresAfter(60);
+            $booksPaginated = $paginator->paginate($books, $offset, $limit);
+
+            return $serializer->serialize($booksPaginated, 'json', ['groups' => 'getBooks']);
+        });
 
         return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
     }
@@ -35,28 +55,32 @@ class BookController extends AbstractController
     #[Route('/api/books/{id}', name: 'app_books_details', methods: ['GET'])]
     public function getBookDetail(Book $book, SerializerInterface $serializer): JsonResponse
     {
-            $jsonBook = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
+        $jsonBook = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
 
-            return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+        return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
     }
 
+    /**
+     * @throws InvalidArgumentException in invalidateTags
+     */
     #[Route('/api/books/{id}', name: 'app_books_delete', methods: ['DELETE'])]
-    public function deleteBook(Book $book, EntityManagerInterface $entityManager): JsonResponse
+    public function deleteBook(Book $book, EntityManagerInterface $entityManager, TagAwareCacheInterface $cache): JsonResponse
     {
-            $entityManager->remove($book);
-            $entityManager->flush();
+        $cache->invalidateTags(['booksCache']);
+        $entityManager->remove($book);
+        $entityManager->flush();
 
-            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
     #[Route('/api/books', name: 'app_books_create', methods: ['POST'])]
     #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour créer un livre.')]
-    public function createBook(Request $request,
-                               SerializerInterface $serializer,
+    public function createBook(Request                $request,
+                               SerializerInterface    $serializer,
                                EntityManagerInterface $entityManager,
-                               UrlGeneratorInterface $urlGenerator,
-                               AuthorRepository $authorRepository,
-                               ValidatorInterface $validator
+                               UrlGeneratorInterface  $urlGenerator,
+                               AuthorRepository       $authorRepository,
+                               ValidatorInterface     $validator
     ): JsonResponse
     {
         $book = $serializer->deserialize($request->getContent(), Book::class, 'json');
@@ -81,12 +105,16 @@ class BookController extends AbstractController
         return new JsonResponse($jsonBook, Response::HTTP_CREATED, ['Location' => $location], true);
     }
 
+    /**
+     * @throws InvalidArgumentException in invalidateTags
+     */
     #[Route('/api/books/{id}', name: 'app_books_update', methods: ['PATCH'])]
-    public function updateBook(Request $request,
-                               SerializerInterface $serializer,
-                               Book $currentBook,
+    public function updateBook(Request                $request,
+                               SerializerInterface    $serializer,
+                               Book                   $currentBook,
                                EntityManagerInterface $entityManager,
-                               ValidatorInterface $validator
+                               ValidatorInterface     $validator,
+                               TagAwareCacheInterface $cache
     ): JsonResponse
     {
         $updatedBook = $serializer->deserialize(
@@ -101,6 +129,7 @@ class BookController extends AbstractController
             return new JsonResponse($serializer->serialize($errors, 'json'), Response::HTTP_BAD_REQUEST, [], true);
         }
 
+        $cache->invalidateTags(['booksCache']);
         $entityManager->persist($updatedBook);
         $entityManager->flush();
 
